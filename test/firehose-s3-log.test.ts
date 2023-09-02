3573aa971ce8093e86b1b2914942d6d927e165a7
mark <mark@watana.be>
2023-09-02 10:10:12 +0900

    Initial commit

---
+++ test/firehose-s3-log.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FirehoseS3Log from '../lib/firehose-s3-log-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/firehose-s3-log-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FirehoseS3Log.FirehoseS3LogStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
